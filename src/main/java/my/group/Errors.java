5af4341bb00fb379eac3b9fe767c2d08fc843714
Zhyriutun Dmytro <dum4uk92@gmail.com>
2023-03-16 21:06:49 +0200

    Add object Errors for creating json errors

---
+++ src/main/java/my/group/Errors.java
@@ -0,0 +1,12 @@
+package my.group;
+
+public class Errors {
+    private final String errors;
+    Errors(String errors){
+        this.errors=errors;
+    }
+
+    public String getErrors() {
+        return errors;
+    }
+}
